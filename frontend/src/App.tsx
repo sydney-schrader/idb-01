50b5401d19730bbf5f30e08b1609deeaf48a0680
Jamie Wong <jamiewong@jamieMac.local>
2023-09-24 20:33:58 -0500

    created basic nav bar. Closes #9

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <>
+    <Nav />
+      <h1>LAHomeLessHelper</h1>
+    </>
+    
+  );
+}
+
+export default App;
