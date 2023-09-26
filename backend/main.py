2de7fc7dba1d0ae5497ac77ab97a7521fb16862b
Zachary Voltz <zachary@voltz.us>
2023-09-25 20:22:24 -0500

    Create Routes for Instances Resolves #18
    Coauthored with @pmarathi

--- backend/main.py
+++ backend/main.py
@@ -12,15 +12,39 @@ def shelters():
     shelters = query_APIs.query_API("shelters")
     return render_template("shelters.html", data=shelters)
 
+@app.route("/shelters/<name>")
+def specific_shelter(name):
+    shelters = query_APIs.query_API("shelters")
+    for shelter in shelters:
+        if shelter["Name"] == name:
+            return render_template("specific_shelter.html", data=shelter)
+    return "<h1>Error 404 Not Found</h1>"
+
 @app.route("/cities")
 def cities():
     cities = query_APIs.query_API("cities")
     return render_template("cities.html", data=cities)
 
+@app.route("/cities/<name>")
+def specific_city(name):
+    cities = query_APIs.query_API("cities")
+    for city in cities:
+        if city["CSA_Label"] == name:
+            return render_template("specific_city.html", data=city)
+    return "<h1>Error 404 Not Found</h1>"
+
 @app.route("/medicare")
 def medicare():
     medicare = query_APIs.query_API("medicare")
     return render_template("medicare.html", data=medicare)
+
+@app.route("/medicare/<name>")
+def specific_medicare(name):
+    medicares = query_APIs.query_API("medicare")
+    for medicare in medicares:
+        if medicare["Name"] == name:
+            return render_template("specific_medicare.html", data=medicare)
+    return "<h1>Error 404 Not Found</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
