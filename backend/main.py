2859d286e5757c57b679d68e646eb8e797ea5659
Zachary Voltz <zachary@voltz.us>
2023-09-25 20:19:32 -0500

    Create Routes for Models Resolves #16
    Coauthored with @pmarathi

--- backend/main.py
+++ backend/main.py
@@ -7,5 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template("tmp_home.html")
 
+@app.route("/shelters")
+def shelters():
+    shelters = query_APIs.query_API("shelters")
+    return render_template("shelters.html", data=shelters)
+
+@app.route("/cities")
+def cities():
+    cities = query_APIs.query_API("cities")
+    return render_template("cities.html", data=cities)
+
+@app.route("/medicare")
+def medicare():
+    medicare = query_APIs.query_API("medicare")
+    return render_template("medicare.html", data=medicare)
+
 if __name__ == "__main__":
     app.run(debug=True)
