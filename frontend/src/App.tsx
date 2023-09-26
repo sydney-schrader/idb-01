ccc3b16bd98b8b499b9fadde434d56d464c93f03
Sydney Schrader <sydneyschrader@wireless-10-147-90-82.public.utexas.edu>
2023-09-26 10:57:24 -0500

    background image and basic title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,21 @@
 import './App.css';
 import Nav from './components/Nav/Nav';
 
+
+
 function App() {
-  return (
-    <>
-    <Nav />
-      <h1>LAHomeLessHelper</h1>
-    </>
-    
-  );
+    return (
+      <>
+      <Nav />
+      <div className="background">
+         <h1 className="h1">
+          Los Angeles <br/> Homeless Helper
+          </h1>
+      </div>
+       
+      </>
+    );
 }
 
 export default App;
+
