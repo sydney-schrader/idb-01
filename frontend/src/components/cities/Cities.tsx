23b24995f827117909c544febff79af3d2dc6d8e
Jamie Wong <jamiewong@jamieMac.local>
2023-10-15 17:54:31 -0500

    created instance pages for each city, formatting is wrong and so is image, data is fine. #58

--- frontend/src/components/cities/Cities.tsx
+++ frontend/src/components/cities/Cities.tsx
@@ -5,7 +5,9 @@ import Button from 'react-bootstrap/Button'
 import agourahills from '../../assets/agoura-hills.jpeg'
 import alhambra from '../../assets/alhambra.jpeg'
 import arcadia from '../../assets/arcadia.jpeg'
-import axios from "axios";  
+import axios from "axios"; 
+import styles from './Cities.module.css' 
+import { Box } from "@mui/material";
 
 const Cities: React.FC<{}> = () => {
   
@@ -21,80 +23,19 @@ const Cities: React.FC<{}> = () => {
       .then((response) => { 
           console.log(response.data);
           setCityData(response.data);
-          console.log(cityData[0].CSA_Label) });
+          //console.log(cityData[0]["CSA_Label"]) 
+        });
    
   }, []);
-
+  
+  // notes: idk how the img works yet, want it to come from google api
   const renderCard = (card: any, index: any) => {
-
-  }
-
-
-    return (
-        <Container>
-        <Col>
-            <h1>Cities in Los Angeles</h1>
-        </Col>
-        <Row>
-        <div> {cityData.length} Cities </div>
-          <Col>
-            <Card style={{ alignItems: 'center' }}>
+    return(
+      <Card style={{ alignItems: 'center'}} key={index}>
               <Card.Title className='header-1'>
-                <b>City of Agoura Hills</b>
-              </Card.Title>
-              <img
-                src={agourahills}
-                alt=""
-                className='card-image-top'
-                style={{
-                  width: '100%',
-                }}
-              ></img>
-              <Card.Body>
-              <p>
-                Unsheltered population: 2<br/>
-                Sheltered population: 0 <br/>
-                Total homeless population: 2 <br/>
-                Square miles of city: 5.42772257254032 <br/>
-                Density of total homeless population: 0.368478670984089
-            </p>
-                <Button name='href' href='/cities/city1' className='card-link'>
-                City of Agoura Hills
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ alignItems: 'center' }}>
-              <Card.Title className='header-1'>
-                <b>City of Alhambra</b>
-              </Card.Title>
-              <img
-                src={alhambra}
-                alt=""
-                className='card-image-top'
-                style={{
-                  width: '100%',
-                }}
-              ></img>
-              <Card.Body>
-              <p>
-                Unsheltered population: 32 <br/>
-                Sheltered population: 14 <br/>
-                Total homeless population: 46 <br/>
-                Square miles of city: 7.63002893846662 <br/>
-                Density of total homeless population: 6.02881068616818 
-                </p>
-                <Button name='href' href='/cities/city2' className='card-link'>
-                  View City of Alhambra
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ alignItems: 'center' }}>
-              <Card.Title className='header-1'>
-                <b>City of Arcadia</b>
+                <b>
+                  {card.CSA_Label}
+                </b>
               </Card.Title>
               <img
                 src={arcadia}
@@ -105,20 +46,33 @@ const Cities: React.FC<{}> = () => {
                 }}
               ></img>
               <Card.Body>
+              
               <p>
-                Unsheltered population: 68 <br/>
-                Sheltered population: 0 <br/>
-                Total homeless population: 68 <br/>
-                Square miles of city: 10.2404110274238<br/>
-                Density of total homeless population: 6.64035846001651 
+                Unsheltered population: {card.Total_Unsheltered_Pop} <br/>
+                Sheltered population: {card.Total_Sheltered_Pop} <br/>
+                Total homeless population: {card.Total_Pop} <br/>
+                Square miles of city: {card.Square_Miles}<br/>
+                Density of total homeless population: {card.Density_Total} 
             </p>
                 <Button name='href' href='/cities/city3' className='card-link'>
                   View City of Arcadia
                 </Button>
               </Card.Body>
             </Card>
-          </Col>
-        </Row>
+    )
+  }
+
+
+    return (
+      <Container>
+        <Col>
+            <h1>Cities in Los Angeles</h1>
+        </Col>
+        <div> {cityData.length} Cities </div>
+        
+          {cityData.map(renderCard)}
+        
+        
       </Container>
     );
     
