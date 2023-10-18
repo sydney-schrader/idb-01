a400091b3817b861ff9760e67235ac2192685dc9
Zachary Voltz <zachary@voltz.us>
2023-10-18 15:06:56 -0500

    Implement Backend Pagination Implements #73

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,24 @@ from flask import *
 from flask_cors import CORS
 import query_APIs
 import query_database
+import math
 
 app = Flask(__name__)
 CORS(app)
+ITEMS_PER_PAGE = 9
 
 @app.route("/api/shelters")
-def shelters():
+@app.route("/api/shelters/")
+@app.route("/api/shelters/<page>")
+def shelters(page=None):
     shelters = query_database.query_shelters()
     shelters = [shelter.to_dict() for shelter in shelters]
-    return shelters
+    if page is None:
+        return shelters
+    num_pages = math.ceil(len(shelters) / ITEMS_PER_PAGE)
+    page = int(page) % num_pages
+    start = ITEMS_PER_PAGE*page
+    return [num_pages] + shelters[start:start+ITEMS_PER_PAGE]
 
 @app.route("/api/shelters/<name>")
 def specific_shelter(name):
@@ -18,21 +27,35 @@ def specific_shelter(name):
     return shelter.to_dict()
 
 @app.route("/api/cities")
-def cities():
+@app.route("/api/cities/")
+@app.route("/api/cities/<page>")
+def cities(page=None):
     cities = query_database.query_cities()
     cities = [city.to_dict() for city in cities]
-    return cities
+    if page is None:
+        return cities
+    num_pages = math.ceil(len(cities) / ITEMS_PER_PAGE)
+    page = int(page) % num_pages
+    start = ITEMS_PER_PAGE*page
+    return [num_pages] + cities[start:start+ITEMS_PER_PAGE]
 
-@app.route("/api/cities/<name>")
+@app.route("/api/city/<name>")
 def specific_city(name):
     city = query_database.query_city(name)
     return city.to_dict()
 
-@app.route("/api/medicare")
-def medicare():
+@app.route("/api/medicares")
+@app.route("/api/medicares/")
+@app.route("/api/medicares/<page>")
+def medicare(page=None):
     medicares = query_database.query_medicares()
     medicares = [medicare.to_dict() for medicare in medicares]
-    return medicares
+    if page is None:
+        return medicares
+    num_pages = math.ceil(len(medicares) / ITEMS_PER_PAGE)
+    page = int(page) % (num_pages+1)
+    start = ITEMS_PER_PAGE*page
+    return [num_pages] + medicares[start:start+ITEMS_PER_PAGE]
 
 @app.route("/api/medicare/<name>")
 def specific_medicare(name):
