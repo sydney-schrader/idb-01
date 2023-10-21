7b8d51b5c80a550222a53a326f75cf81b79cee6c
Sydney Schrader <sydneyschrader@sydneys-mbp-2.lan>
2023-10-21 01:09:19 -0500

    test changes

--- frontend/selenium_tests/acceptance_tests.py
+++ frontend/selenium_tests/acceptance_tests.py
@@ -49,25 +49,25 @@ class LosAngelesHomelessHelperTest(unittest.TestCase):
         self.assertTrue(lower_title.is_displayed())
 
     def test_explore_cities_link(self):
-        explore_cities_button = self.driver.find_element(By.NAME, 'homeCities')
+        explore_cities_button = self.driver.find_element(By.CSS_SELECTOR, "button[href = '/cities']")
         self.assertIsNotNone(explore_cities_button)
         self.driver.execute_script("arguments[0].click();", explore_cities_button)
         self.assertIn('/cities', self.driver.current_url)
 
     def test_explore_resources_link(self):
-        explore_resources_button = self.driver.find_element(By.NAME, 'homeResources')
+        explore_resources_button = self.driver.find_element(By.CSS_SELECTOR, "button[href = '/resources']")
         self.assertIsNotNone(explore_resources_button)
         self.driver.execute_script("arguments[0].click();", explore_resources_button)
         self.assertIn('/resources', self.driver.current_url)
 
     def test_explore_medical_options_link(self):
-        explore_medical_button = self.driver.find_element(By.NAME, 'homeMedical')
+        explore_medical_button = self.driver.find_element(By.CSS_SELECTOR, "button[href = '/medical']")
         self.assertIsNotNone(explore_medical_button)
         self.driver.execute_script("arguments[0].click();", explore_medical_button)
         self.assertIn('/medical', self.driver.current_url)
 
     def test_nav_and_cities(self):
-        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/cities']")
+        link = self.driver.find_element(By.CSS_SELECTOR, "nav a[href='/cities']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/cities', self.driver.current_url)
@@ -75,17 +75,17 @@ class LosAngelesHomelessHelperTest(unittest.TestCase):
     def test_city_connected_instance(self):
         self.driver.get('https://www.lacountyhomelesshelper.me/cities')
         #just test one
-        link = self.driver.find_element(By.NAME, 'City of Agoura Hills')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/cities/City of Agoura Hills']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/cities/City%20of%20Agoura%20Hills', self.driver.current_url)
-        link = self.driver.find_element(By.NAME, 'backCities')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/cities']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/cities', self.driver.current_url)
 
     def test_nav_and_resources(self):
-        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/resources']")
+        link = self.driver.find_element(By.CSS_SELECTOR, "nav a[href='/resources']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/resources', self.driver.current_url)
@@ -93,17 +93,17 @@ class LosAngelesHomelessHelperTest(unittest.TestCase):
     def test_resources_connected_instance(self):
         self.driver.get('https://www.lacountyhomelesshelper.me/resources')
         #just test one
-        link = self.driver.find_element(By.NAME, '1736 Family Crisis Center')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/resources/1736 Family Crisis Center']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/resources/1736%20Family%20Crisis%20Center', self.driver.current_url)
-        link = self.driver.find_element(By.NAME, 'backResources')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/resources']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/resources', self.driver.current_url)
             
     def test_nav_and_medical(self):
-        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/medical']")
+        link = self.driver.find_element(By.CSS_SELECTOR, "nav a[href='/medical']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/medical', self.driver.current_url)
@@ -111,11 +111,11 @@ class LosAngelesHomelessHelperTest(unittest.TestCase):
     def test_medical_connected_instance(self):
         self.driver.get('https://www.lacountyhomelesshelper.me/medical')
         #just test one
-        link = self.driver.find_element(By.NAME, 'Alafia Mental Health Center')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/medical/Alafia Mental Health Center']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/medical/Alafia%20Mental%20Health%20Center', self.driver.current_url)
-        link = self.driver.find_element(By.NAME, 'backMedical')
+        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/medical']")
         self.assertIsNotNone(link)
         self.driver.execute_script("arguments[0].click();", link)
         self.assertIn('/medical', self.driver.current_url)
