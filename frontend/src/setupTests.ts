e257a80f6c596a62c38374d7f4ef316daad0ec65
Jamie Wong <jamiewong@jamieMac.local>
2023-12-01 22:23:58 -0600

    jest

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jest.setup.ts
+global.beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation((message?: string) => {
+      if (message && message.includes("Failed prop type: The prop `textToHighlight` is marked as required in `Highlighter`, but its value is `undefined`.")) {
+        return;
+      }
+      console.error(message);
+    });
+  });
+  
+  global.afterAll(() => {
+    (console.error as jest.Mock).mockRestore();
+  });
+  
