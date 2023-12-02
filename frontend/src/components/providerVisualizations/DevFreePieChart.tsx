665cbf1a506bf8e415d81abb198b4580879630bc
Jamie Wong <jamiewong@jamieMac.local>
2023-12-01 18:13:46 -0600

    created first developer visualization

---
+++ frontend/src/components/providerVisualizations/DevFreePieChart.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"; 
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
+
+// Define the data type
+type PieChartData = {
+  name: string;
+  value: number;
+};
+
+// "api_url": "https://api.chiworks.me/resources"
+
+const DevFreePieChart: React.FC = () => {
+  // Sample data
+
+  const [resourcesData, setResourcesData] = useState<any[]>([]);
+  const [freeCountData, setFreeCountData] = useState<any[]>([]);
+
+  // Colors for each slice
+  const COLORS = ['#979ab9', '#79ac4f', '#80ccfa', '#e27d8a', '#6d5c4b'];
+
+
+  useEffect(() => {
+      axios.get(`https://api.chiworks.me/resources`)
+      .then(async (response) => {
+        const updatedData = await Promise.all(response.data.map(async (resource: any) => {
+          return resource;
+        }));
+        setResourcesData(updatedData);
+      });  
+  }, []);
+
+  var freeCount = 0;
+  for(let i = 0; i < resourcesData.length; i++) {
+    if(resourcesData[i].price == "Free") {
+        freeCount++;
+    }
+  }
+  const data: PieChartData[] = [
+    { name: 'Free Resources', value: (freeCount / resourcesData.length) * 100 },
+    { name: 'Not Free', value: ((resourcesData.length - freeCount) / resourcesData.length) * 100},
+  ];
+
+
+
+  
+  return (
+    <>
+      <ResponsiveContainer width="100%" height={500}>
+        <PieChart>
+          
+          <Pie
+            dataKey="value"
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={250}
+            fill="#8884d8"
+            //label={(entry) => `(${entry.payload.percentage.toFixed(2)}%)`} // Display city name and value as label
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+                onMouseEnter={() => {
+                  // Add hover effect, e.g., change color
+                  // You can customize this behavior based on your needs
+                  console.log(`Hovered over ${entry.name}`);
+                }}
+                onMouseLeave={() => {
+                  // Remove hover effect
+                  console.log(`Left ${entry.name}`);
+                }}
+              />
+            ))}
+          </Pie>
+          <Tooltip formatter={(value, name, props) => `${props.payload.value.toFixed(2)}%`} />
+        </PieChart>
+      </ResponsiveContainer>
+
+      {/* Additional content */}
+      <div>
+        {/* You can add other information or components here */}
+      </div>
+    </>
+  );
+};
+
+export default DevFreePieChart;
--- frontend/src/components/providerVisualizations/ProviderVisualizations.tsx
+++ frontend/src/components/providerVisualizations/ProviderVisualizations.tsx
@@ -9,18 +9,36 @@ import {
     Skeleton,
   } from "@mui/material";
 
+  import DevFreePieChart from "./DevFreePieChart";
+
 function ProviderVisualizationPage(props: any) {
+    // pie chart
     // resources by percentage that are free
+
     // resources dialect
+
     // job postings last update over time
     // job posting salary range
     
 
     return (
+        <Container>
         <Stack justifyContent="center" direction="column" textAlign="center">
             <h1>Chiworks Visualizations</h1>
         </Stack>
-
+        <Stack justifyContent="center" direction="column" textAlign="center">
+            <h3>Percentage of Free Resources</h3>
+        </Stack>
+       <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+        sx={{ padding: "30px" }}
+       >
+        <DevFreePieChart />
+       </Stack>
+        
+        </Container>
     );
 }
 
